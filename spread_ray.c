ef9563069299953a7612ed3b9c2507352652e326
Seongmin Sim <sesim@c10r1s14.42seoul.kr>
2022-10-17 14:11:06 +0900

    refact: in_shadow has a problem #8

--- spread_ray.c
+++ spread_ray.c
@@ -6,7 +6,7 @@
 /*   By: sesim <sesim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 17:49:42 by sesim             #+#    #+#             */
-/*   Updated: 2022/10/17 13:29:08 by sesim            ###   ########.fr       */
+/*   Updated: 2022/10/17 14:08:08 by sesim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 static int	_ray_at_obj(t_object *obj, t_ray ray, t_moment *spot)
 {
-	const int	model_type = PL | SP | CY | CN | CP;
+	// const int	model_type = PL | SP | CY | CN | CP;
 	int			hit_result;
 
 	hit_result = FALSE;
-	if (!(obj->type & model_type))
-		return (hit_result);
-	else if (obj->type & (PL | CP))
+	if (obj->type & (PL | CP))
 		hit_result = ray_at_plane(obj, ray, spot);
 	else if (obj->type & SP)
 		hit_result = ray_at_sphere(obj, ray, spot);
@@ -28,6 +26,8 @@ static int	_ray_at_obj(t_object *obj, t_ray ray, t_moment *spot)
 		hit_result = ray_at_cylinder(obj, ray, spot);
 	else if (obj->type & CN)
 		hit_result = ray_at_cone(obj, ray, spot);
+	if (hit_result == FALSE)
+		return (hit_result);
 	spot->albedo = obj->albedo;
 	return (hit_result);
 }
@@ -39,11 +39,13 @@ int	is_ray_hit(t_object *obj, t_ray ray, t_moment *spot)
 	hit_flag = FALSE;
 	while (obj)
 	{
-		hit_flag = _ray_at_obj(obj, ray, spot);
-		if (hit_flag)
+		if (_ray_at_obj(obj, ray, spot))
+		{
+			hit_flag = TRUE;
 			spot->tmax = spot->t;
+			printf("spot->tmax = %lf\n", spot->tmax);
+		}
 		obj = obj->next;
 	}
-	printf("hit flag : %d\n", hit_flag);
 	return (hit_flag);
 }
